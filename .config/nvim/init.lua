21fde1fdfec33a28e74424cd6a1e0e543573cb7b
Diego Frattini <dgfrattini@gmail.com>
2021-11-25 01:30:45 +0000

    nvim-tree keybindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -153,7 +153,9 @@ require'nvim-tree'.setup {
     auto_resize = true
   }
 }
-vim.api.nvim_set_keymap('n', '<C-e>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
 
 -- Telescope
 require('telescope').setup {
