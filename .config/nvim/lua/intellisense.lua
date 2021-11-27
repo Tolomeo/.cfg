820a97d5a85cd1c63bcb126978f1b274e69e2efe
Diego Frattini <dgfrattini@gmail.com>
2021-11-27 12:38:57 +0000

    Nvim coc keybindings

--- .config/nvim/lua/intellisense.lua
+++ .config/nvim/lua/intellisense.lua
@@ -5,6 +5,18 @@ vim.cmd([[
   let g:coc_global_extensions = ["coc-json", "coc-yaml", "coc-html", "coc-svg", "coc-css", "coc-cssmodules", "coc-tsserver", "coc-diagnostic", "coc-eslint", "coc-prettier", "coc-sumneko-lua"]
 ]])
 -- Keybindings
--- Prettier format buffer
-vim.api.nvim_set_keymap('n', '<leader>b', ':CocCommand prettier.formatFile<CR>', { noremap = true, silent = true })
-
+-- see https://github.com/albingroen/quick.nvim/blob/main/lua/coc-config.lua
+vim.api.nvim_set_keymap("n", "<leader>.", "<Plug>(coc-codeaction)", {})
+vim.api.nvim_set_keymap("n", "<leader>l", ":CocCommand eslint.executeAutofix<CR>", {})
+vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
+vim.api.nvim_set_keymap("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
+vim.api.nvim_set_keymap("n", "<leader>f", ":CocCommand prettier.formatFile<CR>", {noremap = true})
+vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<TAB>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
+vim.api.nvim_set_keymap("i", "<S-TAB>","pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
+vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? coc#_select_confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
+vim.o.hidden = true
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.updatetime = 300
--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -9,3 +9,37 @@ vim.cmd [[colorscheme nord]]
 require('lualine').setup({
   options = { theme = 'nord' }
 })
+
+-- Gitsigns
+require('gitsigns').setup {
+  signs = {
+    add = { hl = 'GitGutterAdd', text = '+' },
+    change = { hl = 'GitGutterChange', text = '~' },
+    delete = { hl = 'GitGutterDelete', text = '_' },
+    topdelete = { hl = 'GitGutterDelete', text = '‾' },
+    changedelete = { hl = 'GitGutterChange', text = '~' },
+  },
+}
+
+-- NvimTree
+require'nvim-tree'.setup {
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = true,
+  auto_close = false,
+  hijack_cursor = true,
+  update_focused_file = {
+    enable = true
+  },
+  diagnostics = {
+    enable = true
+  },
+  view = {
+    side = 'left',
+    auto_resize = true
+  }
+}
+vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
