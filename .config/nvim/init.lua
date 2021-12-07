c1df4d59935130f1ee194a548832b41bb9f7790f
Diego Frattini <dgfrattini@gmail.com>
2021-12-07 23:27:05 +0000

    Nvim defaults split

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+require('defaults')
 -- see https://github.com/wbthomason/packer.nvim#bootstrapping
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
---
+++ .config/nvim/lua/defaults.lua
@@ -0,0 +1,48 @@
+	--Incremental live completion (note: this is now a default on master)
+	vim.o.inccommand = 'nosplit'
+
+	--Set highlight on search
+	vim.o.hlsearch = true
+
+	--Make line numbers default
+	vim.wo.number = true
+
+	--Do not save when switching buffers (note: this is now a default on master)
+	vim.o.hidden = true
+
+	--Enable mouse mode
+	vim.o.mouse = 'a'
+
+	--Enable break indent
+	vim.o.breakindent = true
+
+	--Save undo history
+	vim.opt.undofile = true
+
+	--Case insensitive searching UNLESS /C or capital in search
+	vim.o.ignorecase = true
+	vim.o.smartcase = true
+
+	--Decrease update time
+	vim.o.updatetime = 250
+	vim.wo.signcolumn = 'yes'
+
+	--Set colorscheme (order is important here)
+	vim.o.termguicolors = true
+
+	--Indent size
+	vim.o.shiftwidth = 2
+	vim.o.tabstop = 2
+	vim.o.autoindent = true
+	vim.o.smartindent = true
+
+	--Remap space as leader key
+	vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+	vim.g.mapleader = ' '
+	vim.g.maplocalleader = ' '
+
+	-- Killing netrw
+	vim.g.netrw_banner = 0
+	vim.g.netrw_menu = 0
+	vim.g.loaded_netrw = 1
+	vim.g.loaded_netrwPlugin = 1
