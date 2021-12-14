fcc6fd122f157aec6a9c3c708b641edaae2269e5
Diego Frattini <dgfrattini@gmail.com>
2021-12-14 22:23:25 +0000

    Nvim config lualine plugin recover

--- .config/nvim/lua/modules/theme.lua
+++ .config/nvim/lua/modules/theme.lua
@@ -2,10 +2,10 @@ local M = {}
 
 M.plugins = {
 	'shaunsingh/nord.nvim',
-	--[[ {
+	{
   'nvim-lualine/lualine.nvim',
    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  } ]]
+  }
 }
 
 function M.setup()
@@ -16,7 +16,34 @@ function M.setup()
 	vim.cmd [[colorscheme nord]]
 
 	-- Statusbar
-	-- require('lualine').setup()
+	require'lualine'.setup {
+		options = {
+			icons_enabled = true,
+			theme = 'auto',
+			component_separators = { left = '/', right = '/'},
+			section_separators = { left = '', right = ''},
+			disabled_filetypes = {},
+			always_divide_middle = true,
+		},
+		sections = {
+			lualine_a = {'mode'},
+			lualine_b = {'branch', { 'diff', colored = false }, { 'diagnostics', sources = { 'coc'}, colored = false, update_in_insert = true } },
+			lualine_c = {'filename'},
+			lualine_x = {'encoding', 'fileformat', 'filetype'},
+			lualine_y = {'progress'},
+			lualine_z = {'location'}
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {'filename'},
+			lualine_x = {'location'},
+			lualine_y = {},
+			lualine_z = {}
+		},
+		tabline = {},
+		extensions = {}
+	}
 end
 
 return M
