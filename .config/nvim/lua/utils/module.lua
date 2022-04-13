e31a2a0df19b169e781508e8d5f97a1c001e6de2
Diego Frattini <dgfrattini@gmail.com>
2022-04-13 15:09:45 +0100

    Nvim module refactor wip

--- .config/nvim/lua/utils/module.lua
+++ .config/nvim/lua/utils/module.lua
@@ -1,15 +1,31 @@
-local Module = {
-	plugins = {},
-	modules = {}
-}
+local Module = {}
+
+local decorate = function(module_definition)
+	local module_setup = module_definition.setup
+
+	function module_definition:setup(...)
+		module_setup(module_definition, ...)
+
+		for _, child_module in pairs(module_definition.modules) do
+			child_module.setup(child_module, ...)
+		end
+	end
+
+	return module_definition
+end
 
 function Module:new(m)
 	m = m or {} -- create object if user does not provide one
 	setmetatable(m, self)
 	self.__index = self
-	return m
+
+	return decorate(m)
 end
 
-function Module:setup() end
+Module.setup = function() end
+
+Module.plugins = {}
+
+Module.modules = {}
 
 return Module
