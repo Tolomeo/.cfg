2046c973cb5e265a9cd3bdbe03452903f6392b13
Diego Frattini <dgfrattini@gmail.com>
2022-04-27 19:08:46 +0100

    Nvim lazygit integration wip

--- .config/nvim/lua/interface/window.lua
+++ .config/nvim/lua/interface/window.lua
@@ -13,25 +13,39 @@ local Window = Module:new({
 	plugins = {},
 })
 
-function Window.modal(options)
-	local buffer = options[1]
-	local on_resize = options.on_resize
-	local on_resized = options.on_resized
-
+function Window._get_modal_config()
 	local width = math.ceil(math.min(vim.o.columns, math.max(80, vim.o.columns - 20)))
 	local height = math.ceil(math.min(vim.o.lines, math.max(20, vim.o.lines - 10)))
 	local col = (math.ceil(vim.o.columns - width) / 2) - 1
 	local row = (math.ceil(vim.o.lines - height) / 2) - 1
-	local window = vim.api.nvim_open_win(
-		buffer,
-		true,
-		vim.tbl_extend("force", {
-			col = col,
-			row = row,
-			width = width,
-			height = height,
-		}, defaults.modal)
-	)
+	return vim.tbl_extend("force", {
+		col = col,
+		row = row,
+		width = width,
+		height = height,
+	}, defaults.modal)
+end
+
+function Window.modal(options)
+	local buffer = options[1]
+	local window = vim.api.nvim_open_win(buffer, true, Window._get_modal_config())
+	local on_vim_resized = function()
+		if not vim.api.nvim_win_is_valid(window) then
+			return
+		end
+
+		local updatedConfig = Window._get_modal_config()
+
+		if options.on_resize then
+			options.on_resize(updatedConfig)
+		end
+
+		vim.api.nvim_win_set_config(window, updatedConfig)
+
+		if options.on_resized then
+			options.on_resized(updatedConfig)
+		end
+	end
 
 	au.group({
 		"Interface.Modal",
@@ -40,32 +54,7 @@ function Window.modal(options)
 				"VimResized",
 				nil,
 				-- TODO: debounce this
-				function()
-					if not vim.api.nvim_win_is_valid(window) then
-						return
-					end
-
-					local updatedWidth = math.ceil(math.min(vim.o.columns, math.max(80, vim.o.columns - 20)))
-					local updatedHeight = math.ceil(math.min(vim.o.lines, math.max(20, vim.o.lines - 10)))
-					local updatedCol = (math.ceil(vim.o.columns - width) / 2) - 1
-					local updatedRow = (math.ceil(vim.o.lines - height) / 2) - 1
-					local updatedConfig = vim.tbl_extend("force", {
-						col = updatedCol,
-						row = updatedRow,
-						width = updatedWidth,
-						height = updatedHeight,
-					}, defaults.modal)
-
-					if on_resize then
-						on_resize(updatedConfig)
-					end
-
-					vim.api.nvim_win_set_config(window, updatedConfig)
-
-					if on_resized then
-						on_resized(updatedConfig)
-					end
-				end,
+				on_vim_resized,
 				buffer = buffer,
 			},
 		},
