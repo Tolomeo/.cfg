d90366da9259b55890a270145b8dbaa424f0f65f
Diego Frattini <dgfrattini@gmail.com>
2022-05-07 00:52:20 +0100

    Nvim config fn doc comments

--- .config/nvim/lua/_shared/fn.lua
+++ .config/nvim/lua/_shared/fn.lua
@@ -1,5 +1,10 @@
 local Fn = {}
 
+--- Executes a user-supplied "reducer" callback function on each element of the table indexed with a numeric key, in order, passing in the return value from the calculation on the preceding element
+---@param tbl table the table to loop against
+---@param func function the reducer callback
+---@param acc any the accumulator initial value
+---@return any
 function Fn.ireduce(tbl, func, acc)
 	for i, v in ipairs(tbl) do
 		acc = func(acc, v, i)
@@ -7,6 +12,11 @@ function Fn.ireduce(tbl, func, acc)
 	return acc
 end
 
+--- Executes a user-supplied "reducer" callback function on each key element of the table indexed with a string key, in order, passing in the return value from the calculation on the preceding element
+---@param tbl table the table to loop against
+---@param func function the reducer callback
+---@param acc any the accumulator initial value
+---@return any
 function Fn.kreduce(tbl, func, acc)
 	for i, v in pairs(tbl) do
 		if type(i) == "string" then
@@ -16,6 +26,11 @@ function Fn.kreduce(tbl, func, acc)
 	return acc
 end
 
+--- Executes a user-supplied "reducer" callback function on each element of the table, in order, passing in the return value from the calculation on the preceding element
+---@param tbl table the table to loop against
+---@param func function the reducer callback
+---@param acc any the accumulator initial value
+---@return any
 function Fn.reduce(tbl, func, acc)
 	for i, v in pairs(tbl) do
 		acc = func(acc, v, i)
