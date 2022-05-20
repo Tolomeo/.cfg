5f1867afe2be4071f6a8f067d30ceb5456b3b895
Diego Frattini <dgfrattini@gmail.com>
2022-05-20 18:02:21 +0100

    Nvim coc to native lsp wip

--- .config/nvim/lua/editor/language.lua
+++ .config/nvim/lua/editor/language.lua
@@ -21,84 +21,194 @@ Language.plugins = {
 	-- { "<CR>", modules.editor.language.confirm_suggestion }
 ) ]]
 
-Language.setup = function()
-	local runtime_path = vim.split(package.path, ';')
-	table.insert(runtime_path, "lua/?.lua")
-	table.insert(runtime_path, "lua/?/init.lua")
-	local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-	local on_attach = function()
-		key.nmap(
-			{ "<leader>k", vim.lsp.buf.hover, buffer = 0 },
-			-- { "<leader>K", vim.lsp.buf.document_symbol, buffer = 0 },
-			{ "<leader>gr", vim.lsp.buf.references, buffer = 0 },
-			{ "<leader>gd", vim.lsp.buf.definition, buffer = 0 },
-			{ "<leader>gD", vim.lsp.buf.declaration, buffer = 0 },
-			{ "<leader>gt", vim.lsp.buf.type_definition, buffer = 0 },
-			{ "<leader>gi", vim.lsp.buf.implementation, buffer = 0 },
-			{ "<leader>b", vim.lsp.buf.formatting, buffer = 0 },
-			{ "<leader>r", vim.lsp.buf.rename, buffer = 0 },
-			{ "<leader>dj", vim.diagnostic.goto_next, buffer = 0 },
-			{ "<leader>dk", vim.diagnostic.goto_prev, buffer = 0 },
-			-- TODO: this probaly shouldn't be here
-			{ "<leader>dl", "<cmd>Telescope diagnostics<Cr>", buffer = 0 },
-			{ "<C-Space>", vim.lsp.buf.code_action, buffer = 0 }
-		-- { "<leader>B", modules.editor.language.eslint_fix },
-		-- { "<leader>dl", modules.editor.language.show_diagnostics },
-		)
-
-		au.group({
-			"OnCursorHold",
+local languages = {
+	bash = {
+		syntax = { "bash" },
+		server = {
 			{
-				{
-					"CursorHold",
-					0,
-					vim.lsp.buf.document_highlight
-				},
-				{
-					"CursorHoldI",
-					0,
-					vim.lsp.buf.document_highlight
-				},
-				{
-					"CursorMoved",
-					0,
-					vim.lsp.buf.clear_references
-				},
-			},
-		})
-	end
+				name = "bashls"
+			}
+		}
+	},
+	lua = {
+		syntax = "lua",
+		server = {
+			{
+				name = "sumneko_lua",
+				settings = function(base_settings)
+					local runtime_path = vim.split(package.path, ';')
+					table.insert(runtime_path, "lua/?.lua")
+					table.insert(runtime_path, "lua/?/init.lua")
 
-	require('lspconfig').sumneko_lua.setup({
-		capabilities = capabilities,
-		settings = {
-			Lua = {
-				runtime = {
-					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-					version = 'LuaJIT',
-					-- Setup your lua path
-					path = runtime_path,
-				},
-				diagnostics = {
-					-- Get the language server to recognize the `vim` global
-					globals = { 'vim' },
-				},
-				workspace = {
-					-- Make the server aware of Neovim runtime files
-					library = vim.api.nvim_get_runtime_file("", true),
-				},
-				-- Do not send telemetry data containing a randomized but unique identifier
-				telemetry = {
-					enable = false,
-				},
+					return vim.tbl_extend("force", base_settings, {
+						settings = {
+							Lua = {
+								runtime = {
+									-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+									version = 'LuaJIT',
+									-- Setup your lua path
+									path = runtime_path,
+								},
+								diagnostics = {
+									-- Get the language server to recognize the `vim` global
+									globals = { 'vim' },
+								},
+								workspace = {
+									-- Make the server aware of Neovim runtime files
+									library = vim.api.nvim_get_runtime_file("", true),
+								},
+								-- Do not send telemetry data containing a randomized but unique identifier
+								telemetry = {
+									enable = false,
+								},
+							},
+						}
+					})
+				end
+			}
+		}
+	},
+	htmlcss = {
+		syntax = { "html", "css", "scss" },
+		server = {
+			{
+				name = "html",
+				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#html
+				settings = function(base_settings)
+					base_settings.capabilities.textDocument.completion.completionItem.snippetSupport = true
+					return base_settings
+				end
 			},
+			{
+				name = "cssls",
+				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#cssls
+				settings = function(base_settings)
+					base_settings.capabilities.textDocument.completion.completionItem.snippetSupport = true
+					return base_settings
+				end
+			},
+			{
+				name = "emmet_ls"
+			}
 		},
-		on_attach = on_attach
-	})
+	},
+	docker = {
+		syntax = "dockerfile",
+		server = {
+			{
+				name = "dockerls",
+			}
+		}
+	},
+	json = {
+		syntax = { "json", "jsonc" },
+		server = {
+			{
+				name = "jsonls",
+				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#jsonls
+				settings = function(base_settings)
+					base_settings.capabilities.textDocument.completion.completionItem.snippetSupport = true
+					return base_settings
+				end
+			}
+		}
+	},
+	yaml = {
+		syntax = "yaml",
+		server = {
+			{
+				name = "yamlls",
+				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#yamlls
+				settings = function(base_settings)
+					return vim.tbl_extend("force", base_settings, {
+						settings = {
+							schemas = {
+								["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+							}
+						}
+					})
+				end
+			}
+		}
+	},
+	javascript = {
+		syntax = { "javascript", "typescript", "tsx" },
+		server = {
+			{
+				name = "tsserver"
+			},
+			{
+				name = "cssmodules_ls"
+			},
+			{
+				name = "eslint"
+			}
+		},
+	},
+}
 
-	require('lspconfig').tsserver.setup({
-		capabilities = capabilities,
-		on_attach = on_attach
-	})
+Language.setup = function()
+	local lspconfig = require('lspconfig')
+	local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+	for _, language in pairs(languages) do
+		if not language.server then goto continue end
+
+		for _, language_server in ipairs(language.server) do
+			local name, settings = language_server.name, language_server.settings or function(base_settings) return base_settings end
+
+			lspconfig[name].setup(settings({
+				capabilities = capabilities,
+				on_attach = function(client)
+					print(vim.inspect(client))
+
+					key.nmap(
+						{ "<leader>k", vim.lsp.buf.hover, buffer = 0 },
+						-- { "<leader>K", vim.lsp.buf.document_symbol, buffer = 0 },
+						{ "<leader>gr", vim.lsp.buf.references, buffer = 0 },
+						{ "<leader>gd", vim.lsp.buf.definition, buffer = 0 },
+						{ "<leader>gD", vim.lsp.buf.declaration, buffer = 0 },
+						{ "<leader>gt", vim.lsp.buf.type_definition, buffer = 0 },
+						{ "<leader>gi", vim.lsp.buf.implementation, buffer = 0 },
+						{ "<leader>b", vim.lsp.buf.formatting, buffer = 0 },
+						{ "<leader>r", vim.lsp.buf.rename, buffer = 0 },
+						{ "<leader>dj", vim.diagnostic.goto_next, buffer = 0 },
+						{ "<leader>dk", vim.diagnostic.goto_prev, buffer = 0 },
+						-- TODO: this probaly shouldn't be here
+						{ "<leader>dl", "<cmd>Telescope diagnostics<Cr>", buffer = 0 },
+						{ "<C-Space>", vim.lsp.buf.code_action, buffer = 0 }
+					-- { "<leader>B", modules.editor.language.eslint_fix },
+					-- { "<leader>dl", modules.editor.language.show_diagnostics },
+					)
+
+					if (client.resolved_capabilities.document_highlight) then
+						au.group({
+							"OnCursorHold",
+							{
+								{
+									"CursorHold",
+									0,
+									vim.lsp.buf.document_highlight
+								},
+								{
+									"CursorHoldI",
+									0,
+									vim.lsp.buf.document_highlight
+								},
+								{
+									"CursorMoved",
+									0,
+									vim.lsp.buf.clear_references
+								},
+							},
+						})
+					end
+				end
+			}))
+		end
+
+		::continue::
+	end
 
 	require("nvim-treesitter.configs").setup({
 		ensure_installed = {
