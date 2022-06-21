0437f5b9335751540793a637e0ca8a57e2972599
Diego Frattini <dgfrattini@gmail.com>
2022-06-21 01:14:47 +0100

    Nvim terminal module refactor wip

--- .config/nvim/lua/terminal/init.lua
+++ .config/nvim/lua/terminal/init.lua
@@ -37,24 +37,23 @@ Terminal._setup_keymaps = function()
 	key.nmap({
 		"<leader>t",
 		function()
-			Terminal:next()
+			Terminal:cycle("forward")
 		end,
-	})
-
-	key.nmap({
+	}, {
 		"<leader>T",
 		function()
+			Terminal:cycle("backward")
+		end,
+	}, {
+		"<C-t>",
+		function()
 			Terminal:create()
+			vim.api.nvim_command("startinsert")
 		end,
 	})
 end
 
 Terminal._setup_commands = function()
-	-- In the terminal emulator, insert mode becomes the default mode
-	-- see https://github.com/neovim/neovim/issues/8816
-	-- NOTE: there are some caveats and related workarounds documented at the link
-	-- TODO: enter insert mode even when the buffer reloaded from being hidden
-	-- also, no line numbers in the terminal
 	au.group({
 		"Terminal",
 		{
@@ -62,10 +61,17 @@ Terminal._setup_commands = function()
 				"TermOpen",
 				"term://*",
 				function(autocmd)
-					Terminal:register({ buffer = autocmd.buf, file = autocmd.file })
+					local buffer, file = autocmd.buf, autocmd.file
+					-- No numbers
 					vim.cmd("setlocal nonumber norelativenumber")
+					-- vim.api.nvim_buf_set_option(buffer, "number", false)
+					-- vim.api.nvim_buf_set_option(buffer, "relativenumber", false)
+					-- Unlisting
+					vim.api.nvim_buf_set_option(buffer, "buflisted", false)
 					-- Allow closing a process directly from normal mode
 					key.nmap({ "<C-c>", "i<C-c>", buffer = autocmd.buf })
+
+					Terminal:register({ buffer = buffer, file = file })
 				end,
 			},
 			{
@@ -77,11 +83,6 @@ Terminal._setup_commands = function()
 			},
 		},
 	})
-
-	-- TODO: verify if possible to do this in lua
-	vim.cmd([[
-			:command! EditConfig :tabedit ~/.config/nvim
-		]])
 end
 
 Terminal.jobs = {}
@@ -95,7 +96,6 @@ Terminal.register = validator.f.arguments({
 	.. function(self, job)
 		table.insert(self.jobs, job.buffer)
 		self.jobs[tostring(job.buffer)] = Job:new(job)
-		vim.api.nvim_buf_set_option(job.buffer, "buflisted", false)
 		--NOTE: Changing the buffer name would require to dynamically change every job buffer name on creation/deletion of a job
 		--[[ vim.api.nvim_buf_set_name(
 		job.buffer,
@@ -124,104 +124,66 @@ Terminal.unregister = validator.f.arguments({
 	end
 end
 
+Terminal._get_buffer_job = validator.f.arguments({
+	validator.f.equal(Terminal),
+	"number",
+}) .. function(self, buffer_handler)
+	local job = self.jobs[tostring(buffer_handler)]
+	return job and job or nil
+end
+
 Terminal._get_window_job = validator.f.arguments({
 	validator.f.equal(Terminal),
 	"number",
 }) .. function(self, window_handler)
-	local buffer_handler = vim.api.nvim_win_get_buf(window_handler)
-	local job = self.jobs[tostring(buffer_handler)]
-
-	if job then
-		return job
-	end
-
-	return nil
+	return self:_get_buffer_job(vim.api.nvim_win_get_buf(window_handler))
 end
 
-function Terminal:visible_jobs()
-	local windows = vim.api.nvim_list_wins()
-	return fn.reduce(windows, function(visible_jobs, window_handler)
-		local job = self:_get_window_job(window_handler)
-
-		if not job then
-			return visible_jobs
-		end
-
-		local visible_job = vim.tbl_extend("force", {}, job, { window = window_handler })
-		table.insert(visible_jobs, visible_job)
-
-		return visible_jobs
-	end, {})
-end
-
-function Terminal:jump()
-	local visible_jobs = self:visible_jobs()
-
-	if #visible_jobs < 1 then
-		Terminal:next()
-		return
-	end
-
-	local current_buffer = vim.api.nvim_get_current_buf()
-	local current_window = vim.api.nvim_get_current_win()
-	local current_buffer_job = self.jobs[tostring(current_buffer)]
-
-	if current_buffer_job and #visible_jobs == 1 then
-		return
-	end
-
-	if current_buffer_job then
-		local current_job_window_index = fn.find_index(visible_jobs, function(visible_job)
-			return visible_job.window == current_window
-		end)
-		local next_job_window_index = visible_jobs[current_job_window_index + 1] and current_job_window_index + 1 or 1
-		local next_job_window = visible_jobs[next_job_window_index]
-		vim.api.nvim_set_current_win(next_job_window.window)
-		return
-	end
-
-	local next_job_window = visible_jobs[1]
-	vim.api.nvim_set_current_win(next_job_window.window)
+function Terminal:open_current()
+	vim.api.nvim_command("buffer " .. self.jobs[self.current])
+	print(string.format("Job %d/%d", self.current, #self.jobs))
 end
 
 function Terminal:create()
 	return vim.api.nvim_command("terminal")
 end
 
-function Terminal:next()
-	local jobs_count = #self.jobs
+Terminal.cycle = validator.f.arguments({ validator.f.equal(Terminal), validator.f.one_of({ "forward", "backward" }) })
+	.. function(self, direction)
+		local jobs_count = #self.jobs
 
-	if jobs_count < 1 then
-		print("No running jobs found")
-		return
-	end
+		if jobs_count < 1 then
+			print("No running jobs found")
+			return
+		end
 
-	local current_buffer = vim.api.nvim_get_current_buf()
-	local current_buffer_job = self.jobs[tostring(current_buffer)]
+		local current_buffer = vim.api.nvim_get_current_buf()
+		local current_buffer_job = self.jobs[tostring(current_buffer)]
 
-	if current_buffer_job and jobs_count == 1 then
-		print("Job 1/1")
-		return
-	end
+		if not current_buffer_job then
+			self:open_current()
+			return
+		end
 
-	if current_buffer_job then
-		local current_buffer_job_index = fn.find_index(self.jobs, function(registered_job)
+		if jobs_count == 1 then
+			print("Only 1 job present")
+			return
+		end
+
+		local current_job_index = fn.find_index(self.jobs, function(registered_job)
 			return registered_job == current_buffer_job.buffer
 		end)
-		local next_job_index = self.jobs[current_buffer_job_index + 1] and current_buffer_job_index + 1 or 1
+		local next_job_index = ({
+			forward = function(current_index)
+				return self.jobs[current_index + 1] and current_index + 1 or 1
+			end,
+			backward = function(current_index)
+				return self.jobs[current_index - 1] and current_index - 1 or #self.jobs
+			end,
+		})[direction](current_job_index)
 
-		vim.api.nvim_command("buffer " .. tostring(self.jobs[next_job_index]))
-		-- TODO: replace with emitting an event
-		-- this stopinsert shouldn't be here as it refers to a specific way to use the terminals
-		-- replace with self:emit("jobs:next") and letting subscribers execute business logic
-		vim.api.nvim_command("stopinsert")
 		self.current = next_job_index
-		print(string.format("Job %d/%d", self.current, jobs_count))
-		return
+		self:open_current()
 	end
-
-	vim.api.nvim_command("buffer " .. self.jobs[self.current])
-	print(string.format("Job %d/%d", self.current, jobs_count))
-end
 
 return Module:new(Terminal)
