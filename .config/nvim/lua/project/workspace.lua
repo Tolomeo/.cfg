745d4ed4ffd56bb88cb60259ac271420f12a162d
Diego Frattini <dgfrattini@gmail.com>
2023-06-20 12:34:02 +0100

    Nvim config workspace wip

--- .config/nvim/lua/project/workspace.lua
+++ .config/nvim/lua/project/workspace.lua
@@ -41,6 +41,72 @@ function Workspace:setup()
 	})
 end
 
+function Workspace:on_vim_enter()
+	local args = vim.fn.argv()
+	local cwd = vim.fn.fnamemodify(vim.loop.cwd(), ":p")
+	local initial_tab = tb.current()
+
+	tb.update({ initial_tab, vars = { workspace = cwd } })
+	bf.create({
+		name = cwd,
+		vars = { workspaces = { initial_tab } },
+		options = { modifiable = false, readonly = true, buflisted = true },
+	})
+
+	local directory_args = fn.imap(
+		fn.ifilter(args, function(arg)
+			local file_stat = fs.statSync(arg)
+
+			if not file_stat then
+				return false
+			end
+
+			return file_stat.type == "directory"
+		end),
+		function(dir_arg)
+			return vim.fn.fnamemodify(dir_arg, ":p")
+		end
+	)
+	local file_args = fn.imap(
+		fn.ifilter(args, function(arg)
+			local file_stat = fs.statSync(arg)
+
+			if not file_stat then
+				return false
+			end
+
+			return file_stat.type == "file"
+		end),
+		function(file_arg)
+			return vim.fn.fnamemodify(file_arg, ":p")
+		end
+	)
+	--TODO: Take care of not yet existing args
+
+	fn.ieach(directory_args, function(directory_path)
+		self:create(directory_path)
+	end)
+
+	fn.ieach(file_args, function(file_path)
+		self:create_from_file(file_path)
+	end)
+
+	tb.go_to(1)
+	self:on_tab_enter()
+end
+
+function Workspace:on_tab_enter()
+	local tab = tb.get_current({ vars = { "workspace" } })
+
+	self:display_workspace_buffers(tab)
+
+	if not tab.vars.workspace then
+		return
+	end
+
+	tb.cd(tab.vars.workspace)
+end
+
 function Workspace:on_tab_leave()
 	local ws = tb.get_current({ vars = { "workspace" } })
 
@@ -100,115 +166,6 @@ function Workspace:on_ws_closed(closed_ws)
 	end)
 
 	self:on_tab_enter()
-
-	--[[ fn.ieach(ws_buffers, function(buffer)
-		local buffer_workspaces = fn.ifilter(buffer.vars.workspaces, function(buffer_workspace)
-			return buffer_workspace ~= ws.handle
-		end)
-
-		if #buffer_workspaces < 1 then
-			--TODO: handle buf modified cancel
-			bf.delete({ buffer.handle })
-			return
-		end
-
-		bf.update({ buffer.handle, vars = { workspaces = buffer_workspaces } })
-	end) ]]
-end
-
-function Workspace:get_all()
-	return fn.ifilter(tb.get_list({ vars = { "workspace" } }), function(tab)
-		return tab.vars.workspace ~= nil
-	end)
-end
-
-function Workspace:on_term_open(evt)
-	local buffer = bf.get({ evt.buf })
-
-	self:term_to_workspace(buffer)
-end
-
-function Workspace:term_to_workspace(buffer)
-	local current_ws = tb.get_current({ vars = { "workspace" } })
-
-	bf.update({
-		buffer.handle,
-		vars = {
-			workspaces = { current_ws.handle },
-		},
-	})
-end
-
-function Workspace:on_vim_enter()
-	local args = vim.fn.argv()
-	local cwd = vim.fn.fnamemodify(vim.loop.cwd(), ":p")
-	local initial_tab = tb.current()
-
-	tb.update({ initial_tab, vars = { workspace = cwd } })
-	bf.create({
-		name = cwd,
-		vars = { workspaces = { initial_tab } },
-		options = { modifiable = false, readonly = true, buflisted = true },
-	})
-
-	local directory_args = fn.imap(
-		fn.ifilter(args, function(arg)
-			local file_stat = fs.statSync(arg)
-
-			if not file_stat then
-				return false
-			end
-
-			return file_stat.type == "directory"
-		end),
-		function(dir_arg)
-			return vim.fn.fnamemodify(dir_arg, ":p")
-		end
-	)
-	local file_args = fn.imap(
-		fn.ifilter(args, function(arg)
-			local file_stat = fs.statSync(arg)
-
-			if not file_stat then
-				return false
-			end
-
-			return file_stat.type == "file"
-		end),
-		function(file_arg)
-			return vim.fn.fnamemodify(file_arg, ":p")
-		end
-	)
-	--TODO: Take care of not yet existing args
-
-	fn.ieach(directory_args, function(directory_path)
-		self:create(directory_path)
-	end)
-
-	fn.ieach(file_args, function(file_path)
-		self:create_from_file(file_path)
-	end)
-
-	tb.go_to(1)
-	self:on_tab_enter()
-end
-
-function Workspace:create_from_file(file_path)
-	local file_dir = pt.dirname({ file_path })
-	local root_dir = self:find_root(file_dir)
-	local workspaces = self:get_by_root(root_dir)
-
-	if #workspaces < 1 then
-		self:create(root_dir)
-		workspaces = self:get_by_root(root_dir)
-	end
-
-	bf.update({
-		bf.get_handle_by_name({ file_path }),
-		vars = { workspaces = fn.imap(workspaces, function(ws)
-			return ws.handle
-		end) },
-	})
 end
 
 function Workspace:on_buf_new(evt)
@@ -270,27 +227,19 @@ function Workspace:on_file_buf_new(buffer)
 	self:on_tab_enter()
 end
 
-function Workspace:on_tab_enter()
-	local tab = tb.get_current({ vars = { "workspace" } })
+function Workspace:on_term_open(evt)
+	local current_ws = self:get_current()
 
-	self:display_workspace_buffers(tab)
-
-	if not tab.vars.workspace then
+	if not current_ws then
 		return
 	end
 
-	tb.cd(tab.vars.workspace)
-end
-
-function Workspace:display_workspace_buffers(tab)
-	local ws_id = tab.handle
-	local ws_buffers = fn.ifilter(bf.get_all({ vars = { "workspaces" } }), function(buffer)
-		return buffer.vars.workspaces ~= nil
-	end)
-
-	fn.ieach(ws_buffers, function(buffer)
-		bf.update({ buffer.handle, options = { buflisted = fn.iincludes(buffer.vars.workspaces, ws_id) } })
-	end)
+	bf.update({
+		evt.buf,
+		vars = {
+			workspaces = { current_ws.handle },
+		},
+	})
 end
 
 function Workspace:create(root, tab)
@@ -317,9 +266,57 @@ function Workspace:create(root, tab)
 	return tab, dashboard_buffer
 end
 
+function Workspace:create_from_file(file_path)
+	local file_dir = pt.dirname({ file_path })
+	local root_dir = self:find_root(file_dir)
+	local workspaces = self:get_by_root(root_dir)
+
+	if not next(workspaces) then
+		self:create(root_dir)
+		workspaces = self:get_by_root(root_dir)
+	end
+
+	local buffer = bf.get_handle_by_name({ file_path })
+	local buffer_workspaces = fn.imap(workspaces, function(ws)
+		return ws.handle
+	end)
+
+	bf.update({
+		buffer,
+		vars = { workspaces = buffer_workspaces },
+	})
+end
+
+function Workspace:get_all()
+	return fn.ifilter(tb.get_list({ vars = { "workspace" } }), function(tab)
+		return tab.vars.workspace ~= nil
+	end)
+end
+
+function Workspace:get_current()
+	local current_tab = tb.get_current({ vars = { "workspace" } })
+
+	if not current_tab.vars.workspace then
+		return nil
+	end
+
+	return current_tab
+end
+
 function Workspace:get_by_root(root)
 	return fn.ifilter(tb.get_list({ vars = { "workspace" } }), function(ws)
 		return ws.vars.workspace == root
+	end)
+end
+
+function Workspace:display_workspace_buffers(tab)
+	local ws_id = tab.handle
+	local ws_buffers = fn.ifilter(bf.get_all({ vars = { "workspaces" } }), function(buffer)
+		return buffer.vars.workspaces ~= nil
+	end)
+
+	fn.ieach(ws_buffers, function(buffer)
+		bf.update({ buffer.handle, options = { buflisted = fn.iincludes(buffer.vars.workspaces, ws_id) } })
 	end)
 end
 
